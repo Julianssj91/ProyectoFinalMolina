49931b0a8e9daed05c0cbce919c771565a6dd6d4
Julian Molina <ejmolinax91@gmail.com>
2023-02-02 19:18:21 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,85 @@
+
+
+
+$(document).ready(function () {
+    Swal.fire({
+        title: "Bienvenido a mi Pokedex",
+        text: "Aqui podras buscar tu Pokemon favorito por nombre o numero",
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP63KFodIM-yR5YUmOQV3acijeDMZ7JWhz7JD7fdniRA&s",
+        confirmButtonColor: "#FF0000",
+        showConfirmButton: true,
+    });
+    let searchHistory = [];
+
+    $("#searchBtn").click(function () {
+        let pokemonInput = $("#pokemonInput").val();
+
+        $.ajax({
+            type: "GET",
+            url: `https://pokeapi.co/api/v2/pokemon/${pokemonInput}`,
+            success: function (response) {
+
+                let pokemonInfo = {
+                    id: response.id,
+                    name: response.name,
+                    weight: response.weight,
+                    height: response.height,
+                    type: response.types[0].type.name,
+                };
+
+                searchHistory.push(pokemonInfo);
+                localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+
+                $("#pokemonInfo").removeClass("d-none");
+                $("#pokemonImage").attr("src", response.sprites.front_default);
+                $("#pokemonName").text(response.name);
+                $("#pokemonId").text(`ID: ${response.id}`);
+                $("#pokemonWeight").text(`Peso: ${response.weight} kg`);
+                $("#pokemonHeight").text(`Altura: ${response.height} cm`);
+                $("#pokemonType").text(`Tipo: ${response.types[0].type.name}`);
+
+                displaySearchHistory();
+            },
+            error: function () {
+                Swal.fire({
+                    title: "Pokemon no encontrado!",
+                    text: "Quiza has escrito mal su nombre o ID... o quiza sea un Pokemon nunca antes visto!",
+                    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP63KFodIM-yR5YUmOQV3acijeDMZ7JWhz7JD7fdniRA&s",
+                    confirmButtonColor: "#FF0000",
+                    showConfirmButton: true,
+                });
+            },
+        });
+    });
+
+    function displaySearchHistory() {
+        $("#searchHistory").empty();
+
+        for (let i = 0; i < searchHistory.length; i++) {
+            let pokemon = searchHistory[i];
+
+            let row = `
+        <tr>
+          <td>${pokemon.id}</td>
+          <td>${pokemon.name}</td>
+          <td>${pokemon.weight}kg</td>
+          <td>${pokemon.height}cm</td>
+          <td>${pokemon.type}</td>
+        </tr>
+      `;
+
+            $("#searchHistory").append(row);
+        }
+    }
+
+    $("#clearHistory").click(function () {
+        searchHistory = [];
+        localStorage.removeItem("searchHistory");
+        displaySearchHistory();
+    });
+
+    if (localStorage.getItem("searchHistory")) {
+        searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+        displaySearchHistory();
+    }
+});
