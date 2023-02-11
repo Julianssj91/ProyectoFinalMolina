ce3334decdff5558845bb5aeae56fe5d033fd90c
Julian Molina <ejmolinax91@gmail.com>
2023-02-11 16:24:09 -0300

    final

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ $(document).ready(function () {
     Swal.fire({
         title: "Bienvenido a mi Pokedex",
         text: "Aqui podrás buscar tu Pokemon favorito por nombre o número",
-        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP63KFodIM-yR5YUmOQV3acijeDMZ7JWhz7JD7fdniRA&s",
+        imageUrl: "pokeballintro.png",
         confirmButtonColor: "#FF0000",
         showConfirmButton: true,
     });
     let searchHistory = [];
+
+
+
+
 
     $("#searchBtn").click(function () {
         let pokemonInput = $("#pokemonInput").val();
@@ -14,13 +18,16 @@ $(document).ready(function () {
         fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInput}`)
             .then(response => response.json())
             .then(response => {
+                let { id, name, weight, height, type } = response;
+
                 let pokemonInfo = {
-                    id: response.id,
-                    name: response.name,
-                    weight: response.weight,
-                    height: response.height,
+                    id,
+                    name,
+                    weight,
+                    height,
                     type: response.types[0].type.name,
                 };
+
 
                 searchHistory.push(pokemonInfo);
                 localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
@@ -38,13 +45,16 @@ $(document).ready(function () {
             .catch(error => {
                 Swal.fire({
                     title: "Pokemon no encontrado!",
-                    text: "Quizá has escrito mal su nombre o ID... o quizá sea un Pokemon nunca antes visto!",
-                    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP63KFodIM-yR5YUmOQV3acijeDMZ7JWhz7JD7fdniRA&s",
+                    text: "Quizá has escrito mal su nombre o ID... o quizá sea un Pokemon nunca antes visto!😨",
+                    imageUrl: "pokeballintro.png",
                     confirmButtonColor: "#FF0000",
                     showConfirmButton: true,
                 });
             });
     });
+
+
+
 
     function displaySearchHistory() {
         $("#searchHistory").empty();
@@ -56,26 +66,37 @@ $(document).ready(function () {
             <tr>
             <td>${pokemon.id}</td>
             <td>${pokemon.name}</td>
-            <td>${pokemon.weight}kg</td>
-            <td>${pokemon.height}cm</td>
+            <td>${pokemon.weight}</td>
+            <td>${pokemon.height}</td>
             <td>${pokemon.type}</td>
             </tr>`; $("#searchHistory").append(row);
         }
     }
 
+
+
     $("#clearHistory").click(function () {
         searchHistory = [];
         localStorage.removeItem("searchHistory");
         displaySearchHistory();
+
+        Toastify({
+            text: "HISTORIAL BORRADO!",
+            duration: 3000,
+            close: true,
+            gravity: "top",
+            position: 'left',
+            backgroundColor: "linear-gradient(to right, #FF0000, #6b0404)"
+        }).showToast();
     });
+
+    
 
     if (localStorage.getItem("searchHistory")) {
         searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
         displaySearchHistory();
     }
 
+    
+
 });
-
-
-
-
